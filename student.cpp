6303d967de96bd705991291805ea5a733122a5c2
pacejkas <whsosgnsgns@gmail.com>
2021-09-13 16:58:52 -0500

    Update student.cpp
    Removed void Student::Student_gradeinfo()
    Modified each functions

--- student.cpp
+++ student.cpp
@@ -10,31 +10,63 @@ using namespace std;
 
 void Student::Student_info()
 {
-	cout << get_name() << ", " << get_age() << endl;
+	cout << get_name() << ", " << get_age() << ", " << get_gpa() << endl;
 }
 
-void Student::Student_gradeinfo()
+void Student::get_classgrade(string course)
 {
-	cout << get_name() << ", " << get_age() << ", " << get_grade() << endl;
-}
-
-
-
-char Student::get_classgrade(string course)
-{
-	int j = 0;
-	for (unsigned int i = 0; i <= course_list.size(); i++)
+	int j = -1;
+	if (course_list.size() != 0)
 	{
-		if (course == course_list[i])
+		for (unsigned int i = 0; i < course_list.size(); i++)
 		{
-			j = i;
+			if (course == course_list[i])
+			{
+				j = i;
+			}
+		}
+		char grade = grade_list[j];
+		if (j == -1)
+			cout << name << " has not taken " << course << " yet" << endl;
+		else
+			cout << name << " has made " << grade << " on " << course << endl;
+	}
+	else
+		get_course();
+}
+
+void Student::get_course()
+{
+	if (course_list.size() == 0)
+	{
+		cout << name << " has not taken any class yet" << endl;
+	}
+	else
+	{
+		for (unsigned int i = 0; i < course_list.size(); i++)
+		{
+			cout << course_list[i] << endl;
 		}
 	}
-	char grade = grade_list[j];
-	return grade;
 }
 
 double Student::compute_gpa()
 {
-	
+	int total = 0;
+	for (unsigned int i = 0; i < grade_list.size(); i++)
+	{
+		if (grade_list[i] == 'A')
+			total += 4;
+		else if (grade_list[i] == 'B')
+			total += 3;
+		else if (grade_list[i] == 'C')
+			total += 2;
+		else if (grade_list[i] == 'D')
+			total += 1;
+		else
+			total += 0;
+	}
+	double GPA = total / grade_list.size();
+
+	return GPA;
 }
