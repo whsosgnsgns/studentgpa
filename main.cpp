ba96fda1e427cbcfa45552c23e94e024057d4684
pacejkas <whsosgnsgns@gmail.com>
2021-09-12 22:32:37 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+	/*
+	Student chris("Chris", 20);
+	chris.Student_info();
+	cout << chris.get_grade() << endl;
+	chris.add_course_grade("Statics", 'A');
+	Student carl("Carl", 19);
+	cout << chris.get_classgrade("Statics") << endl;
+	cout << carl.get_classgrade("Statics") << endl;
+	*/
+
+
+	return 0;
+}
+
+
+
